df4f1a7076a178888d091813f8d41d43546adeae
Sullivan Wisdom <wiztemple7@gmail.com>
2020-08-07 09:04:16 +0100

    naive string search

--- searching.js
+++ searching.js
@@ -2,7 +2,7 @@
 // 1. Describe what a searching algorithm is
 // 2. Implement linear search on arrays
 // 3. Implement binary search on sorted arrays
-// 4. Implement a naive string searching Algorithme
+// 4. Implement a naive string searching Algorithm
 // 5. Implement the KMP string searching algorithm
 const linearSearch = (arr, val) => {
   for (i = 0; i < arr.length; i++) {
@@ -25,3 +25,16 @@ const binarySearch = (sortedArr, element) => {
   return sortedArr[middle] === element ? middle : -1;
 };
 console.log(binarySearch([2, 3, 5, 7, 8, 12, 23, 27, 32, 37, 42, 46], 50));
+
+const naiveSearch = (long, short) => {
+  let count = 0;
+  for (let i = 0; i <= long.length; i++) {
+    for (let j = 0; j <= short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+};
+
+naiveSearch("lorie loled", "lol");
