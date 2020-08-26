83d7b7dd96af566cec7c7b61dc52fa6d288e774c
Sullivan Wisdom <wiztemple7@gmail.com>
2020-08-26 13:46:07 +0100

    add get and set methods

--- DS/SinglyLinkedList.js
+++ DS/SinglyLinkedList.js
@@ -87,6 +87,24 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 const list = new SinglyLinkedList();
